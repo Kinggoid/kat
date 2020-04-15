7246ea894ab867c49b57f1735c38fcc112538274
Kinggoid <thijmespambox@gmail.com>
2020-04-15 15:47:11 +0200

    egel

---
+++ Mijn prut/src/opdracht/Main.java
@@ -0,0 +1,10 @@
+package opdracht;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        egel ding = new egel("Sonic");
+        System.out.println("naam: " + ding.Soort());
+    }
+
+}
---
+++ Mijn prut/src/opdracht/egel.java
@@ -0,0 +1,11 @@
+package opdracht;
+
+public class egel {
+    private String soort;
+
+    public egel(String srt) {
+        soort = srt;
+    }
+
+    public String Soort() {return soort;}
+}
