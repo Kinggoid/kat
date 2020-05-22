a81ac83f0d417df1e085db2d6c406a63b01ab709
Kinggoid <thijmespambox@gmail.com>
2020-05-22 15:02:19 +0200

    Opdracht 3a

--- Mijn prut/src/Opdracht3a/Main.java
+++ Mijn prut/src/Opdracht3a/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         AlfabetCode deze = new AlfabetCode();
         deze.sortedLetters(); // Deze werkt het beste van de twee
-        deze.iets(); // Bij deze gebruik ik treeSort maar daar kan ik niet twee dezelfde letters van het alfabet in één set stoppen.
-                     // Dus dan pakt hij gewoon een nieuwe letter en dan gebruikt ie die. Maar dit is niet wat we zouden willen.
+        deze.iets(); // Bij deze gebruik ik treeSort 'want die sort automatisch' maar het is een set waardoor je niet twee dezelfde letters kan sorteren.
 
         System.out.println(deze.list1);
         System.out.println(deze.list2);
